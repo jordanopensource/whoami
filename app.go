d70fd71e19fcb9206d1decf45540f618d81268b1
Baraa Al-Masri <pub@mbaraa.com>
2023-09-19 20:06:47 +0300

    patch: reject empty origin string

--- app.go
+++ app.go
@@ -133,7 +133,7 @@ func handle(next http.HandlerFunc, verbose bool) http.Handler {
 
 func allowCorsHandler(handler http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if origin := r.Header.Get("Origin"); strings.Contains(allowedOrigins, origin) {
+		if origin := r.Header.Get("Origin"); origin != "" && strings.Contains(allowedOrigins, origin) {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
